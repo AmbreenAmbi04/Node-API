e7940c480e15925a633dff07df416a5c84b0ce94
Ambreen <ambreenambi04@gmail.com>
2025-09-09 09:53:10 +0500

    Added A JavaScript Array!

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+app.use(express.json());
+
+let tasks = [
+    { id: 1, text: "Initialize an express server" },
+    { id: 2, text: "Knead the dough" },
+    { id: 3, text: "Complete the tasks" },
+    { id: 4, text: "Fry the donuts" },
+]
